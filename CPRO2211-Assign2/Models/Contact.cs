cd19d68f548b816e65e18b5409c9c93063c0840f
NegrebLC <BergCu2001@gmail.com>
2024-02-13 00:26:23 -0700

    Contact Class Version 1

---
+++ CPRO2211-Assign2/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CPRO2211_Assign2.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        public string Category { get; set; }
+
+        public string Organization { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+    }
+}
